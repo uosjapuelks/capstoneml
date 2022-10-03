3b8cd71a88b1faea330bf12e069625d94ba9d496
Anderson Leong <anderson@Andersons-MacBook-Pro.local>
2022-10-03 17:19:57 +0800

    Testing real data

--- scripts/visualisers.py
+++ scripts/visualisers.py
@@ -37,6 +37,25 @@ def plot_activity(activity, data):
     plt.subplots_adjust(top=0.90)
     plt.show()
 
+def plot_features(activity, data):
+    fig, (ax0, ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10) = plt.subplots(nrows=11, figsize=(20,15), sharex=True)
+    plot_axis(ax0, data['time'], data['max_a'], 'MAX-A')
+    plot_axis(ax1, data['time'], data['min_a'], 'MIN-A')
+    plot_axis(ax2, data['time'], data['range_a'], 'RANGE-A')
+    plot_axis(ax3, data['time'], data['mean_a'], 'MEAN-A')
+    plot_axis(ax4, data['time'], data['std_a'], 'STD-A')
+    plot_axis(ax5, data['time'], data['max_g'], 'MAX-G')
+    plot_axis(ax6, data['time'], data['min_g'], 'MIN-G')
+    plot_axis(ax7, data['time'], data['range_g'], 'RANGE-G')
+    plot_axis(ax8, data['time'], data['mean_g'], 'MEAN-G')
+    plot_axis(ax9, data['time'], data['std_g'], 'STD-G')
+    plot_axis(ax10, data['time'], data['accel_z_peaks'], 'AZ-PEAK')
+    
+    plt.subplots_adjust(hspace=0.2)
+    fig.suptitle(activity)
+    plt.subplots_adjust(top=0.90)
+    plt.show()
+
 def plot_axis(ax, x, y, title):
     ax.plot(x, y,'g')
     ax.set_title(title)

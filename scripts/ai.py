141ead7a0662fbcb992a488c1089852140cbbe97
Anderson Leong <anderson@u.nus.edu>
2022-11-07 07:23:08 +0000

    changes to work better with external comms

--- scripts/ai.py
+++ scripts/ai.py
@@ -3,20 +3,21 @@ from pynq import allocate
 import numpy as np
 import os
 
-# from filepaths_extComms import paths
-from filepaths import paths
+from filepaths_extComms import paths
+# from filepaths import paths
 from modelling_utils import softmax
 
+BIT = os.path.join(paths.get('BITS_DIR'), 'cnn-mix-moretrng.bit')
 
 class AI_FPGA:
-    def __init__(self, bitstream='cnn_mix_moretrng.bit'):
-        BIT = os.path.join(paths.get('BITS_T_DIR'), bitstream)
-        print(BIT)
-        self.overlay = Overlay(BIT)
-        self.dma = self.overlay.axi_dma_0
+    # def __init__(self, bitstream='cnn-mix-moretrng.bit'):
+        # print(self.BIT)
+        
+    overlay = Overlay(BIT)
+    dma = overlay.axi_dma_0
 
-        self.in_buffer0 = allocate(shape=(120,), dtype=np.float32)
-        self.out_buffer0 = allocate(shape=(5,), dtype=np.float32)
+    in_buffer0 = allocate(shape=(120,), dtype=np.float32)
+    out_buffer0 = allocate(shape=(5,), dtype=np.float32)
 
     ###
     ### TAKE NOTE! Fs MAY CHANGE
@@ -32,10 +33,16 @@ class AI_FPGA:
         test_data = np.array(test_data).reshape(120)
         for i in range(120):
             self.in_buffer0[i]=test_data[i]
-        self.dma.sendchannel.transfer(self.in_buffer0)
-        self.dma.recvchannel.transfer(self.out_buffer0)
-        self.dma.sendchannel.wait()
-        self.dma.recvchannel.wait()
+        try:
+            self.dma.sendchannel.transfer(self.in_buffer0)
+            self.dma.recvchannel.transfer(self.out_buffer0)
+            self.dma.sendchannel.wait()
+            self.dma.recvchannel.wait()
+        except Exception as e:
+            print("\033[32mDMA FACED CHALLENGES, RETRYING overlay begin\033[0m {}".format(e))
+            self.overlay = Overlay(BIT)
+            self.dma = self.overlay.axi_dma_0
+
         for i in range(5):
             chances.append(self.out_buffer0[i])
 
--- scripts/start_detector.py
+++ scripts/start_detector.py
@@ -20,7 +20,8 @@ class Detector:
         self.cols = cols
         self.prev_data = pd.DataFrame(columns=cols)
         self.cur_data = pd.DataFrame(columns=cols)
-        self.fpga = AI_FPGA(model)
+        self.fpga = AI_FPGA()
+        # self.fpga = AI_FPGA(model)
         self.res_ls = [self.fpga.idle_code]
         self.feat_df = pd.DataFrame()
         self.margin = 0
@@ -34,19 +35,21 @@ class Detector:
         self.prev_data=self.cur_data.iloc[:]
 
     # Check if selected threshold is exceeded
-    def check_df_threshold(self, data):
+    def check_df_threshold(self, data, threshold=0.060):
         feat = extract_std_range(data, self.fpga.frame_size)
         max_std = (max(feat['std_a']))
-        return max_std > 0.065
+        return max_std > threshold
 
     # Check for return value
     def checkRetVal(self):
         length = len(self.res_ls)
         ret_val = int(stats.mode(self.res_ls)[0][0])
         if ret_val==0 and length>15:
+            print("[START_DETECTOR]:",self.res_ls)
             # self.res_ls = [self.fpga.idle_code]
             return ret_val
-        elif length > 4 and ret_val!=0:
+        elif length > self.ideal_len and ret_val!=0:
+            print("[START_DETECTOR]:",self.res_ls)
             # self.res_ls = [self.fpga.idle_code]
             return ret_val
         else:
@@ -55,6 +58,7 @@ class Detector:
     # margins help with acccidental classification of idle randomly
     def checkMargins(self):
         if self.margin==0:
+            # print("\r[START DETECTOR]: ", self.res_ls, end="")
             ret_val = self.checkRetVal()
             self.res_ls = [self.res_ls[-1]]
         else:
@@ -63,7 +67,7 @@ class Detector:
         return ret_val
 
 # Function - Constantly called by External Comms after initializing class
-    def eval_data(self, raw_data, errMarg=1, sensitivity=0.70):
+    def eval_data(self, raw_data, errMarg=1, sensitivity=0.70, threshold=0.060, ideal_len=3):
         self.counter+=1
         self.process_data(raw_data) # Return df of raw data
         if self.counter < 20:
@@ -74,27 +78,19 @@ class Detector:
         
         data = self.cur_data.copy()
         data = scale_vals(data)
-        pass_threshold = self.check_df_threshold(data)
+        pass_threshold = self.check_df_threshold(data, threshold)
 
         # Std Activated -> get chances and res_fpga
         if pass_threshold:
             chance_fpga, res_fpga = self.fpga.fpga_predict(data)
             if res_fpga!=self.fpga.idle_code:
             # NOTE on actual fpga, run softmax first
-            # if chances greater than 0.88 append
+            # if chances greater than sensitivity append
                 if chance_fpga[res_fpga] > sensitivity or res_fpga==0:
                     # Reset margin to errMarg
                     self.margin=errMarg
                     self.res_ls.append(res_fpga)
-        #         else:
-        #             ret_val = self.checkMargins()
-        #             return ret_val
-        #     else: # the res_fpga IS IDLE
-        #         ret_val = self.checkMargins()
-        #         return ret_val
-        # else:
-        #     ret_val = self.checkMargins()
-        #     return ret_val
+        self.ideal_len = ideal_len
         ret_val = self.checkMargins()
         return ret_val
 
